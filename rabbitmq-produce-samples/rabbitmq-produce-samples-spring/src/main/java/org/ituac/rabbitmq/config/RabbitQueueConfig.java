abf27934f9bdb686097992f68bb1725001642e65
AnDunWXR <wuxinrui@iandun.cn>
2019-10-30 11:05:43 +0800

    springmq -produce

--- rabbitmq-produce-samples/rabbitmq-produce-samples-spring/src/main/java/org/ituac/rabbitmq/config/RabbitQueueConfig.java
+++ rabbitmq-produce-samples/rabbitmq-produce-samples-spring/src/main/java/org/ituac/rabbitmq/config/RabbitQueueConfig.java
@@ -154,6 +154,95 @@ public class RabbitQueueConfig extends BaseMqConfig {
 	    return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_ALL_CITY_ISSUE).noargs();
 	}
 
-	
+	/* ---------------------------------------------------- 死信形式 -------------------------------------------- */
+
+	//创建正常业务队列
+	@Bean
+	public Queue mailQueue() {
+		//创建正常队列
+		//return new Queue(TT_ITUAC_MAIL_QUEUE_LETTER,true, false, false, map);
+		return QueueBuilder.durable(TT_ITUAC_MAIL_QUEUE_LETTER)
+				//以下是重点：当变成死信队列时，会转发至 路由为x-dead-letter-exchange及x-dead-letter-routing-key的队列中
+				.withArgument("x-dead-letter-exchange", TT_DEAD_EXCHANGE_LETTER)
+				.withArgument("x-dead-letter-routing-key", ROUTING_KEY_DEAD_LETTER_KEY)
+				.build();
+	}
+
+	// 创建业务交换机
+	@Bean
+	public FanoutExchange mailExchange() {
+		return new FanoutExchange(TT_ITUAC_MAIL_EXCHANGE_LETTER, true, false);
+	}
+
+	// 绑定交换机
+	@Bean
+	Binding bindingExchangeMail(@Qualifier("mailQueue")Queue queue,@Qualifier("mailExchange")FanoutExchange fanoutExchange) {
+		return BindingBuilder.bind(queue).to(fanoutExchange);
+	}
+
+	// 创建死信队列
+	@Bean
+	public Queue deadQueue(){
+		Queue queue = new Queue(TT_DEAD_QUEUE_LETTER, true);
+		return queue;
+	}
+
+	// 创建死信交换机
+	@Bean
+	public DirectExchange deadExchange() {
+		return new DirectExchange(TT_DEAD_EXCHANGE_LETTER, true, false);
+	}
+
+	// 绑定死信队列
+	@Bean
+	public Binding bindingDeadMailExchange(@Qualifier("deadQueue")Queue deadQueue,@Qualifier("deadExchange")DirectExchange deadExchange) {
+		return BindingBuilder.bind(deadQueue).to(deadExchange).with(ROUTING_KEY_DEAD_LETTER_KEY);
+	}
+
+
+
+
+
+
+	/* ---------------------------------- AE（alternate-exchange）形式｜备份交换机 ------------------------------ */
+
+	//AE, 创建正常队列
+	@Bean(name="sourceHotQueue")
+	public Queue sourceHotQueue() {
+		return new Queue(AE_ITUAC_SOURCE_HOT_QUEUE_ISSUE,true);
+	}
+
+	//创建源交换机，并绑定备份交换机
+	@Bean(name="sourceExchange")
+	public DirectExchange sourceExchange() {
+		return ExchangeBuilder.directExchange(AE_ITUAC_SOURCE_EXCHANGE_ISSUE).durable(true)
+				.autoDelete()
+				.withArgument("alternate-exchange", AE_ITUAC_BACK_EXCHANGE_ISSUE).build();	// 源交换机没有匹配到会转发到备交换机
+	}
+
+	//创建备用队列
+	@Bean(name="backHotQueue")
+	public Queue backHotQueue() {
+		return new Queue(AE_ITUAC_BACK_HOT_QUEUE_ISSUE,true);
+	}
+
+	//创建备用交换机
+	@Bean(name="backExchange")
+	public FanoutExchange backExchange() {
+		// 此处的交换机要和 源交换机中 alternate-exchange 参数的值一致
+		return new FanoutExchange(AE_ITUAC_BACK_EXCHANGE_ISSUE);
+	}
+
+	//被路由到的 交换器绑定队列
+	@Bean
+	Binding bindingExchangeGood(@Qualifier("sourceHotQueue")Queue sourceHotQueue,@Qualifier("sourceExchange")DirectExchange sourceExchange) {
+		return BindingBuilder.bind(sourceHotQueue).to(sourceExchange).with(AE_ITUAC_SOURCE_HOT_ROUTING_KEY);
+	}
+
+	//未被路由到的 交换器绑定队列
+	@Bean
+	Binding bindingAeExchange(@Qualifier("backHotQueue")Queue backHotQueue,@Qualifier("backExchange")FanoutExchange backExchange) {
+		return BindingBuilder.bind(backHotQueue).to(backExchange);
+	}
 	
 }
--- rabbitmq-produce-samples/rabbitmq-produce-samples-spring/src/test/SpringRabbitTest.java
+++ rabbitmq-produce-samples/rabbitmq-produce-samples-spring/src/test/SpringRabbitTest.java
@@ -1,4 +1,4 @@
-import org.itauc.rabbitmq.message.MessageMq;
+import org.itauc.rabbitmq.message.*;
 import org.ituac.rabbitmq.Sender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +16,7 @@ public class SpringRabbitTest extends AbstractJUnit4SpringContextTests {
     private static final SimpleDateFormat date = new SimpleDateFormat("HH:mm:ss");
     @Autowired
     private Sender sender;
+
     @Test
     public void sendMessage(){
         // 对象传输，对象必须实现Serializable序列化
@@ -31,4 +32,59 @@ public class SpringRabbitTest extends AbstractJUnit4SpringContextTests {
         }
         System.out.println("发送完毕");
     }
+    @Test
+    public void reportUserCurrentTime() {
+        // 对象传输，对象必须实现Serializable序列化
+        UserMq user = new UserMq();
+        user.setAge(30);
+        user.setId(2);
+        user.setMessage("I'm topic");
+        user.setName("波罗星");
+        user.setSex(2);
+        user.setTime(new Date());
+
+        sender.sendUser(user);
+        System.out.println("now time：" + date.format(new Date()));
+    }
+
+    @Test
+    public void reportCityCurrentTime() {
+        // 对象传输，对象必须实现Serializable序列化
+        CityMq city = new CityMq();
+        city.setId(2);
+        city.setCode("200");
+        city.setMsg("城市重要通知");
+        city.setTime(new Date());
+
+        sender.sendCity(city);
+        System.out.println("now time：" + date.format(new Date()));
+    }
+
+
+
+    @Test
+    public void reportDeadMessageCurrentTime() {
+        // 对象传输，对象必须实现Serializable序列化
+        MailMq mail = new MailMq();
+        mail.setId(1);
+        mail.setTime(new Date());
+        mail.setMail("907788997@qq.com");
+        sender.sendDead(mail);
+        System.out.println("now time：" + date.format(new Date()));
+    }
+
+    @Test
+    public void reportHostCurrentTime() {
+        // 对象传输，对象必须实现Serializable序列化
+        HotMq hot = new HotMq();
+        hot.setId(1);
+        hot.setDescp("博客：blog.ituac.com");
+        hot.setLike(100000);
+        hot.setName("波罗星");
+        sender.sendHotSource(hot);	//发送给正常的
+        hot.setName("波罗星AE测试");
+        sender.sendHotBack(hot);		//发送给坏的
+        System.out.println("now time：" + date.format(new Date()));
+    }
+
 }
